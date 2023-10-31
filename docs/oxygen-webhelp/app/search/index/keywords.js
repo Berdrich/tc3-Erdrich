7896d1c679b626da73b2aca8326e29a59ffa0fd2
Berdrich <brianerdrichwriter@gmail.com>
2023-10-31 15:59:31 -0500

    rough draft 2

---
+++ docs/oxygen-webhelp/app/search/index/keywords.js
@@ -0,0 +1,16 @@
+var keywords=[{w:"Context",p:["p0"]},{w:"Sensitive",p:["p0"]},{w:"Help",p:["p0"]},{w:"Apple",p:["p1","p2","p3"]},{w:"Filling",p:["p1","p2","p7","p8"]},{w:"Ingredients",p:["p1","p5","p7"]},{w:"Directions",p:["p2","p6","p8"]},{w:"for",p:["p2","p6","p8"]},{w:"Pie",p:["p3","p4","p6","p9"]},{w:"Bake",p:["p4"]},{w:"the",p:["p4"]},{w:"Crust",p:["p5","p6"]},{w:"Pumpkin",p:["p7","p8","p9"]}];
+var ph={};
+ph["p0"]=[0, 1, 2];
+ph["p1"]=[3, 4, 5];
+ph["p2"]=[6, 7, 3, 4];
+ph["p3"]=[3, 8];
+ph["p4"]=[9, 10, 8];
+ph["p5"]=[11, 5];
+ph["p6"]=[6, 7, 8, 11];
+ph["p7"]=[12, 4, 5];
+ph["p8"]=[6, 7, 12, 4];
+ph["p9"]=[12, 8];
+var keywordsInfo = {
+    keywords: keywords,
+    ph: ph
+}
---
+++ docs/oxygen-webhelp/app/search/index/link-to-parent.js
@@ -0,0 +1,2 @@
+/*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
+var linkToParent = {12:-1,3:-1,10:12,6:12,11:12,9:3,2:3,4:12,5:3,1:3,8:12,7:3};
