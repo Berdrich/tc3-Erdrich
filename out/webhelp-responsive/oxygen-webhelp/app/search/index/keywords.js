ecba6d2d595d4d2260619eda2c2063b6f82bab66
Berdrich <brianerdrichwriter@gmail.com>
2023-10-31 15:55:25 -0500

    Rough Draft

---
+++ out/webhelp-responsive/oxygen-webhelp/app/search/index/keywords.js
@@ -0,0 +1,20 @@
+var keywords=[{w:"Context",p:["p0"]},{w:"Sensitive",p:["p0"]},{w:"Help",p:["p0"]},{w:"Apple",p:["p1","p2","p3"]},{w:"Filling",p:["p1","p2","p8","p9"]},{w:"Ingredients",p:["p1","p6","p8"]},{w:"Directions",p:["p2","p7","p9"]},{w:"for",p:["p2","p7","p9"]},{w:"Pie",p:["p3","p4","p7"]},{w:"Bake",p:["p4"]},{w:"the",p:["p4"]},{w:"Concept",p:["p5"]},{w:"Crust",p:["p6","p7"]},{w:"Pumpkin",p:["p8","p9"]},{w:"Reference",p:["p10"]},{w:"Task",p:["p11"]},{w:"Topic",p:["p12","p13"]},{w:"My",p:["p13"]},{w:"first",p:["p13"]}];
+var ph={};
+ph["p0"]=[0, 1, 2];
+ph["p1"]=[3, 4, 5];
+ph["p2"]=[6, 7, 3, 4];
+ph["p3"]=[3, 8];
+ph["p4"]=[9, 10, 8];
+ph["p5"]=[11];
+ph["p6"]=[12, 5];
+ph["p7"]=[6, 7, 8, 12];
+ph["p8"]=[13, 4, 5];
+ph["p9"]=[6, 7, 13, 4];
+ph["p10"]=[14];
+ph["p12"]=[16];
+ph["p11"]=[15];
+ph["p13"]=[17, 18, 16];
+var keywordsInfo = {
+    keywords: keywords,
+    ph: ph
+}
---
+++ out/webhelp-responsive/oxygen-webhelp/app/search/index/link-to-parent.js
@@ -0,0 +1,2 @@
+/*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
+var linkToParent = {13:-1,3:-1,11:13,8:3,7:13,2:3,4:13,1:3,5:3,10:3,9:13,12:13};
